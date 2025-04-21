f8517b7eb384ddc95d10828b447463059794ac9f
dika-maulidal <me.dikamaulidal@gmail.com>
2025-04-21 21:47:34 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+
+import robotsTxt from "astro-robots-txt"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://dika-maulidal.github.io/cobarepo',
+  base: '/cobarepo/',
+})
